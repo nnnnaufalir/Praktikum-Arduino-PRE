f4d8623a8e22b3b2ec461c66d1c7791136e04b55
Naufal Ilham Ramadhan <111553406+nnnnaufalir@users.noreply.github.com>
2026-04-08 13:08:21 +0700

    Add Arduino sketch for sequential LED control
    This code controls three LEDs to light up in sequence with a delay.

---
+++ 3. On-Off Lampu Variasi (Minimal 3 Lampu).ino
@@ -0,0 +1,23 @@
+//SESUAIKAN DENGAN RANGKAIAN KALIAN, BISA LEBIH DARI 3 LED, SESUAI SELERA!
+const int led1 = 8, led2 = 9, led3 = 10;
+
+void setup() {
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+}
+
+void loop() {
+  // Nyala bergantian (Running LED)
+  digitalWrite(led1, HIGH);
+  delay(500);
+  digitalWrite(led1, LOW);
+
+  digitalWrite(led2, HIGH);
+  delay(500);
+  digitalWrite(led2, LOW);
+
+  digitalWrite(led3, HIGH);
+  delay(500);
+  digitalWrite(led3, LOW);
+}
